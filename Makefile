ca42b2b7aea80d73246d6be3694d63b99baa8d73
Thomas Backman <serenity@exscape.org>
2011-08-15 12:31:13 +0200

    Add a very simple benchmark (that doesn't even measure speed itself; it's still mostly manual work)

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: keytest
+all: tests
 
 clean:
 	rm bin/keytest
 
-keytest:
+tests:
 	gcc -m64 -std=gnu99 -o bin/tests keyschedule.c aes.c tests.c debug.c -O0 -Wall -Werror
+
+bench:
+	gcc -m64 -std=gnu99 -o bin/bench keyschedule.c aes.c bench.c debug.c -O3 -Wall -Werror
---
+++ bench.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h> /* exit */
+#include <string.h> /* memcmp */
+#include <assert.h>
+#include "keyschedule.h"
+#include "debug.h"
+#include "aes.h"
+
+int main() {
+	uint64_t text[2] = {0};
+	unsigned char *plaintext_ptr = (unsigned char *)text;
+	const unsigned char key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	unsigned char expanded_key[176] = {0};
+	unsigned char ciphertext[16] = {0};
+
+	aes_expand_key(key, expanded_key);
+
+#define NUM_LOOPS 10000000
+	for (int i=0; i < NUM_LOOPS; i++) {
+		text[1]++;
+		aes_encrypt(plaintext_ptr, ciphertext, expanded_key);
+//		print_hex(ciphertext, 16);
+	}
+
+	return 0;
+}
